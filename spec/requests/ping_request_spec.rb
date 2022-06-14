d12a0419328dd04034edbb3048ae8284dfe34fa5
paulkudocs <paul@kudocs.co.uk>
2022-06-14 23:32:52 +0100

    2 fails 1 pass

--- spec/requests/ping_request_spec.rb
+++ spec/requests/ping_request_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe 'Ping Request', type: :request do
     it 'Returns a 401 status code if not logged in' do 
         get '/ping/auth' 
         expect(response).to have_http_status(401)
-       # expect(response.body).to eq('{"error":"Unauthorized"}') 
+   
     end
-  #  it 'Returns a 200 status code if logged in' do 
-    #    user = create(:user) 
-     #   headers = get_headers(user.email) 
-     #   get '/ping/auth', headers: headers 
-      #  expect(response).to have_http_status(200) 
-   # end
+   it 'Returns a 200 status code if logged in' do 
+        user = create(:user) 
+        headers = get_headers(user.email) 
+        get '/ping/auth', headers: headers 
+        expect(response).to have_http_status(200) 
+    end 
 end
